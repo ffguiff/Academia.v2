dc4d2ca0f639a1111599b364e3f0345103c6aadd
Guilherme Ferreira <90099945+ffguiff@users.noreply.github.com>
2023-01-21 16:50:40 -0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,199 @@
+document.getElementById("fundo").style.visibility = "hidden";
+document.getElementById("circulo3").style.visibility = "hidden";
+document.getElementById("img1").style.visibility = "hidden";
+document.getElementById("img2").style.visibility = "hidden";
+document.getElementById("img5").style.visibility = "hidden";
+document.getElementById("img26").style.visibility = "hidden";
+document.getElementById("img27").style.visibility = "hidden";
+document.getElementById("img24").style.visibility = "hidden";
+document.getElementById("img45").style.visibility = "hidden";
+document.getElementById("img49").style.visibility = "hidden";
+document.getElementById("img50").style.visibility = "hidden";
+document.getElementById("img47").style.visibility = "hidden";
+document.getElementById("img41").style.visibility = "hidden";
+document.getElementById("img8").style.visibility = "hidden";
+document.getElementById("img9").style.visibility = "hidden";
+document.getElementById("img11").style.visibility = "hidden";
+document.getElementById("img32").style.visibility = "hidden";
+document.getElementById("img30").style.visibility = "hidden";
+document.getElementById("img35").style.visibility = "hidden";
+document.getElementById("img18").style.visibility = "hidden";
+document.getElementById("img20").style.visibility = "hidden";
+document.getElementById("img22").style.visibility = "hidden";
+document.getElementById("img19").style.visibility = "hidden";
+document.getElementById("img17").style.visibility = "hidden";
+document.getElementById("img16").style.visibility = "hidden";
+document.getElementById("imgX").style.visibility = "hidden";
+//
+document.getElementById("um").style.visibility = "hidden";
+document.getElementById("dois").style.visibility = "hidden";
+//
+function voltar(){
+    document.getElementById("fundo").style.visibility = "hidden";
+    document.getElementById("circulo3").style.visibility = "hidden";
+
+    document.getElementById("fundo").style.visibility = "hidden";
+document.getElementById("circulo3").style.visibility = "hidden";
+document.getElementById("img1").style.visibility = "hidden";
+document.getElementById("img2").style.visibility = "hidden";
+document.getElementById("img5").style.visibility = "hidden";
+document.getElementById("img26").style.visibility = "hidden";
+document.getElementById("img27").style.visibility = "hidden";
+document.getElementById("img24").style.visibility = "hidden";
+document.getElementById("img45").style.visibility = "hidden";
+document.getElementById("img49").style.visibility = "hidden";
+document.getElementById("img50").style.visibility = "hidden";
+document.getElementById("img47").style.visibility = "hidden";
+document.getElementById("img41").style.visibility = "hidden";
+document.getElementById("img8").style.visibility = "hidden";
+document.getElementById("img9").style.visibility = "hidden";
+document.getElementById("img11").style.visibility = "hidden";
+document.getElementById("img32").style.visibility = "hidden";
+document.getElementById("img30").style.visibility = "hidden";
+document.getElementById("img35").style.visibility = "hidden";
+document.getElementById("img18").style.visibility = "hidden";
+document.getElementById("img20").style.visibility = "hidden";
+document.getElementById("img22").style.visibility = "hidden";
+document.getElementById("img19").style.visibility = "hidden";
+document.getElementById("img17").style.visibility = "hidden";
+document.getElementById("img16").style.visibility = "hidden";
+document.getElementById("imgX").style.visibility = "hidden";
+//
+document.getElementById("um").style.visibility = "hidden";
+document.getElementById("dois").style.visibility = "hidden";
+document.getElementById("cinco").style.visibility = "hidden";
+document.getElementById("vinteeSeis").style.visibility = "hidden";
+document.getElementById("vinteeSete").style.visibility = "hidden";
+document.getElementById("vinteeQuatro").style.visibility = "hidden";
+document.getElementById("quarentaeCinco").style.visibility = "hidden";
+document.getElementById("quarentaeNove").style.visibility = "hidden";
+document.getElementById("cinquenta").style.visibility = "hidden";
+document.getElementById("quarentaeSete").style.visibility = "hidden";
+document.getElementById("quarentaeSeteEmPe").style.visibility = "hidden";
+document.getElementById("quarentaeUm").style.visibility = "hidden";
+document.getElementById("oito").style.visibility = "hidden";
+document.getElementById("nove").style.visibility = "hidden";
+document.getElementById("onze").style.visibility = "hidden";
+document.getElementById("trintaeDois").style.visibility = "hidden";
+document.getElementById("trinta").style.visibility = "hidden";
+document.getElementById("trintaeCinco").style.visibility = "hidden";
+document.getElementById("dezoito").style.visibility = "hidden";
+document.getElementById("vinte").style.visibility = "hidden";
+document.getElementById("vinteeDois").style.visibility = "hidden";
+document.getElementById("dezenove").style.visibility = "hidden";
+document.getElementById("dezessete").style.visibility = "hidden";
+document.getElementById("dezesseis").style.visibility = "hidden";
+document.getElementById("abdominal").style.visibility = "hidden";
+
+}
+//
+function gifTreino(numGif){
+ console.log(numGif); 
+ document.getElementById("circulo3").style.visibility = "visible";
+ document.getElementById("fundo").style.visibility = "visible";
+ 
+switch(numGif){
+     case '1':
+         document.getElementById("img1").style.visibility = "visible";
+         document.getElementById("um").style.visibility = "visible";
+         break;
+     case '2':
+         document.getElementById("img2").style.visibility = "visible";
+         document.getElementById("dois").style.visibility = "visible";
+         break;
+     case '5':
+        document.getElementById("img5").style.visibility = "visible";
+        document.getElementById("cinco").style.visibility = "visible";
+        break;
+     case '26':
+        document.getElementById("img26").style.visibility = "visible";
+        document.getElementById("vinteeSeis").style.visibility = "visible";
+        break;        
+     case '27':
+        document.getElementById("img27").style.visibility = "visible";
+        document.getElementById("vinteeSete").style.visibility = "visible";
+        break;        
+     case '24':
+        document.getElementById("img24").style.visibility = "visible";
+        document.getElementById("vinteeQuatro").style.visibility = "visible";
+        break;
+     case 'x':
+        document.getElementById("imgX").style.visibility = "visible";
+        document.getElementById("abdominal").style.visibility = "visible";
+        break;  
+     case '45':
+        document.getElementById("img45").style.visibility = "visible";
+        document.getElementById("quarentaeCinco").style.visibility = "visible";
+        break;   
+     case '49':
+        document.getElementById("img49").style.visibility = "visible";
+        document.getElementById("quarentaeNove").style.visibility = "visible";
+        break;  
+     case '50':
+        document.getElementById("img50").style.visibility = "visible";
+        document.getElementById("cinquenta").style.visibility = "visible";
+        break;    
+     case '47':
+        document.getElementById("img47").style.visibility = "visible";
+        document.getElementById("quarentaeSete").style.visibility = "visible";
+        break;          
+     case '47EmPe':
+        document.getElementById("img47").style.visibility = "visible";
+        document.getElementById("quarentaeSeteEmPe").style.visibility = "visible";
+        break;  
+     case '41':
+        document.getElementById("img41").style.visibility = "visible";
+        document.getElementById("quarentaeUm").style.visibility = "visible";
+        break; 
+     case '8':
+        document.getElementById("img8").style.visibility = "visible";
+        document.getElementById("oito").style.visibility = "visible";
+        break;
+     case '9':
+        document.getElementById("img9").style.visibility = "visible";
+        document.getElementById("nove").style.visibility = "visible";
+        break;      
+     case '11':
+        document.getElementById("img11").style.visibility = "visible";
+        document.getElementById("onze").style.visibility = "visible";
+        break;
+     case '32':
+        document.getElementById("img32").style.visibility = "visible";
+        document.getElementById("trintaeDois").style.visibility = "visible";
+        break;   
+     case '30':
+        document.getElementById("img30").style.visibility = "visible";
+        document.getElementById("trinta").style.visibility = "visible";
+        break;
+     case '35':
+        document.getElementById("img35").style.visibility = "visible";
+        document.getElementById("trintaeCinco").style.visibility = "visible";
+        break;    
+     case '18':
+        document.getElementById("img18").style.visibility = "visible";
+        document.getElementById("dezoito").style.visibility = "visible";
+        break; 
+     case '20':
+        document.getElementById("img20").style.visibility = "visible";
+        document.getElementById("vinte").style.visibility = "visible";
+        break;          
+     case '22':
+        document.getElementById("img22").style.visibility = "visible";
+        document.getElementById("vinteeDois").style.visibility = "visible";
+        break;                
+     case '19':
+        document.getElementById("img19").style.visibility = "visible";
+        document.getElementById("dezenove").style.visibility = "visible";
+        break;        
+     case '17':
+        document.getElementById("img17").style.visibility = "visible";
+        document.getElementById("dezessete").style.visibility = "visible";
+        break;     
+     case '16':
+        document.getElementById("img16").style.visibility = "visible";
+        document.getElementById("dezesseis").style.visibility = "visible";
+        break;                                              
+ }
+ 
+
+}
